4bb2171e45d331ec690b96531b2e100e561ee3e3
Jon Karyo <jkaryo1@jhu.edu>
2016-11-27 23:59:29 -0500

    working hard

--- CacheSimulator/src/CacheSimulator.java
+++ CacheSimulator/src/CacheSimulator.java
@@ -18,9 +18,19 @@ public class CacheSimulator {
     private static final int W_THROUGH = 4;
     private static final int LEAST_RECENT = 5;
     private static final int FILE = 6;
+    private static final int ADDRESS_BYTES = 8;
+    private static final int SIXTEEN = 16;
+    private static final String HEX = "0123456789abcdefABCDEF";
     private int[] arguments;
     private Scanner trace;
     private Map<Long, Map<Long, Boolean>> cache;
+    private int totalLoads = 0;
+    private int totalStores = 0;
+    private int loadHits = 0;
+    private int loadMisses = 0;
+    private int storeHits = 0;
+    private int storeMisses = 0;
+    private int totalCycles = 0;
     /**
      * Constructor.
      * @param args command line arguments
@@ -54,12 +64,57 @@ public class CacheSimulator {
      * @param args int arguments
      */
     private void testValidity(int[] args) {
-        this.testFirstTwo(arguments[NUM_SETS]);
-        this.testFirstTwo(arguments[NUM_BLOCKS]);
-        this.testThird(arguments[NUM_BYTES]);
-        this.testLastThree(arguments[W_ALLOCATE]);
-        this.testLastThree(arguments[W_THROUGH]);
-        this.testLastThree(arguments[LEAST_RECENT]);
+        this.testFirstTwo(this.numSets());
+        this.testFirstTwo(this.numBlocks());
+        this.testThird(this.numBytes());
+        this.testLastThree(this.wAllocate());
+        this.testLastThree(this.wThrough());
+        this.testLastThree(this.leastRecent());
+        if (this.wAllocate() == 0 && this.wThrough() == 0) {
+            this.parameterError();
+        }
+    }
+    /**
+     * Number of sets.
+     * @return numSets
+     */
+    public int numSets() {
+        return arguments[NUM_SETS];
+    }
+    /**
+     * Number of blocks.
+     * @return numBlocks
+     */
+    public int numBlocks() {
+        return arguments[NUM_BLOCKS];
+    }
+    /**
+     * Number of bytes.
+     * @return numBytes
+     */
+    public int numBytes() {
+        return arguments[NUM_BYTES];
+    }
+    /**
+     * Write allocate or not.
+     * @return wAllocate
+     */
+    public int wAllocate() {
+        return arguments[W_ALLOCATE];
+    }
+    /**
+     * Write-through or write-back.
+     * @return wThrough
+     */
+    public int wThrough() {
+        return arguments[W_THROUGH];
+    }
+    /**
+     * Least-recently-used or FIFO.
+     * @return leastRecent
+     */
+    public int leastRecent() {
+        return arguments[LEAST_RECENT];
     }
     /**
      * Tests first 2 args.
@@ -67,8 +122,7 @@ public class CacheSimulator {
      */
     private void testFirstTwo(int i) {
         if (i <= 0 || (i % 2 != 0 && i != 1)) {
-            System.err.println("Invalid parameter.");
-            System.exit(0);
+            this.parameterError();
         }
     }
     /**
@@ -77,8 +131,7 @@ public class CacheSimulator {
      */
     private void testThird(int i) {
         if (i < 2 * 2 || i % 2 != 0) {
-            System.err.println("Invalid parameter.");
-            System.exit(0);
+            this.parameterError();
         }
     }
     /**
@@ -87,9 +140,12 @@ public class CacheSimulator {
      */
     private void testLastThree(int i) {
         if (i != 0 && i != 1) {
-            System.err.println("Invalid parameter.");
-            System.exit(0);
+            this.parameterError();
         }
+    }
+    private void parameterError() {
+        System.err.println("Invalid parameter.");
+        System.exit(0);
     }
     /**
      * Scans through the trace file acting accordingly.
@@ -111,27 +167,27 @@ public class CacheSimulator {
         if (lineScan.hasNext()) {
             command = lineScan.next();
         } else {
-            System.err.println("Invalid trace file.");
-            System.exit(0);
+            this.parseError();
         }
         if (lineScan.hasNext()) {
             address = lineScan.next();
         } else {
-            System.err.println("Invalid trace file.");
-            System.exit(0);
+            this.parseError();
         }
         if (lineScan.hasNext()) {
             lineScan.next();
         } else {
-            System.err.println("Invalid trace file.");
-            System.exit(0);
+            this.parseError();
         }
         if (lineScan.hasNext()) {
-            System.err.println("Invalid trace file.");
-            System.exit(0);
+            this.parseError();
         }
         lineScan.close();
         this.processTrace(command, address);
+    }
+    private void parseError() {
+        System.err.println("Invalid trace file.");
+        System.exit(0);
     }
     /**
      * Processes whether to load or store.
@@ -153,16 +209,67 @@ public class CacheSimulator {
      * @param address to load
      */
     private void load(String address) {
-        System.out.println("Load: " + address);
-        return;
+        this.addressValidity(address);
+        this.totalLoads++;
+        this.processTag(address, false);
     }
     /**
      * Store address.
      * @param address to store
      */
     private void store(String address) {
-        System.out.println("Store: " + address);
+        this.addressValidity(address);
+        this.totalStores++;
+        this.processTag(address, true);
+    }
+    private void processTag(String address, boolean store) {
+        Long decimal = this.hexToDec(address);
+        decimal = decimal
+                >> (int) Math.ceil(Math.log(this.numBytes()) / Math.log(2));
+        int setBits = (int) Math.ceil(Math.log(this.numSets()) / Math.log(2));
+        Long setBitsLong = Integer.toUnsignedLong((int)
+                Math.pow(setBits, 2) - 1);
+        Long setIndex = decimal & setBitsLong;
+        decimal = decimal >> setBits;
+        if (!store) {
+            this.accessCache(setIndex, decimal);
+        }
+    }
+    private void addressValidity(String address) {
+        if (!address.substring(0, 2).equals("0x")
+                && !address.substring(0, 2).equals("0X")) {
+            this.parseError();
+        }
+        address = address.substring(2);
+        if (address.length() != ADDRESS_BYTES) {
+            this.parseError();
+        }
+        for (char c : address.toCharArray()) {
+            if (HEX.indexOf(c) == -1) {
+                this.parseError();
+            }
+        }
+    }
+    private Long hexToDec(String hex) {
+        Long decimal = Long.parseLong(hex, SIXTEEN);
+        return decimal;
+    }
+    private void accessCache(Long setIndex, Long tag) {
         return;
+    }
+
+
+    /**
+     * Prints results.
+     */
+    public void printResults() {
+        System.out.println("Total loads: " + totalLoads);
+        System.out.println("Total stores: " + totalStores);
+        System.out.println("Load hits: " + loadHits);
+        System.out.println("Load misses: " + loadMisses);
+        System.out.println("Store hits: " + storeHits);
+        System.out.println("Store misses: " + storeMisses);
+        System.out.println("Total cycles: " + totalCycles);
     }
     /**
      * Main method.
